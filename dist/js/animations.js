2797dde2474d5cc6eb6875cae2976506da63f2e2
Elias Debaveye <elias.debaveye@student.howest.be>
2021-04-20 18:00:36 +0200

    init

---
+++ dist/js/animations.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+  container: document.querySelector(".c-logo-1"), // Required
+  path: 'logolottie.json', // Required
+  renderer: 'svg/canvas/html', // Required
+  loop: false, // Optional
+  autoplay: true, // Optional
+  name: "Hello World", // Name for future reference. Optional.
+})
